d5b7390989c089924f1459d9bf178f8b86993913
Meheady <hmmehedi55@gmail.com>
2021-07-05 03:14:32 +0600

    update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const play1Score=document.getElementById('playerscore1');
-const play2Score=document.getElementById('playscore2');
-const wscoredisply = document.querySelector('p span');
-const inputscore = document.getElementById('inputscore');
+let play1Score=document.getElementById('playerscore1');
+let play2Score=document.getElementById('playscore2');
+let wscoredisply = document.querySelector('p span');
+let inputscore = document.getElementById('inputscore');
 
 
 const playbtn1 = document.getElementById('PlayerOne');
@@ -9,32 +9,60 @@ const playbtn2 = document.getElementById('PlayerTw');
 const resetbtn = document.getElementById('reset');
 let  p1score = 0;
 let p2score = 0;
-let winscore = 5;
+let winscore = 0;
 let gameover = false;
 
 playbtn1.addEventListener('click', () =>{
   if (!gameover) {
     p1score++;
     
-    if (p1score===winscore) {
-      gameover = true;
-      
-    }
+    winner(p1score,winscore)
     play1Score.textContent=p1score;
   }
+  inputscore.value='';
   
   });
 
   playbtn2.addEventListener('click', () =>{
     if (!gameover) {
       p2score++;
-   
-    if (p2score===winscore) {
-      gameover = true;
-      
-    }
+      winner(p2score,winscore)
+  
     play2Score.textContent=p2score;
     }
-    
+    inputscore.value='';
   });
+
+  function winner(oldscore,winscore) {
+    if (oldscore===winscore) {
+      gameover = true;
+      playbtn1.setAttribute('disabled','disabled');
+      playbtn2.setAttribute('disabled','disabled');
+      playbtn1.style.backgroundColor="red";
+      playbtn2.style.backgroundColor="red";
+      
+    }
+  }
+
+  inputscore.addEventListener('keyup',()=>{
+    winscore =Number(inputscore.value);
+    wscoredisply.textContent=inputscore.value;
+    reset()
+  })
+
+function reset() {
+  p1score=0;
+    p2score=0;
+    gameover = false;
+    play1Score.textContent=0;
+    play2Score.textContent=0;
+    playbtn1.removeAttribute('disabled');
+      playbtn2.removeAttribute('disabled');
+      playbtn1.style.backgroundColor="green";
+      playbtn2.style.backgroundColor="green";
+    
+}
+
+  resetbtn.addEventListener('click',reset);
+
 
